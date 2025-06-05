abd3905f99173d6e663e369d791f8b53f2c7b77f
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2025-06-05 17:00:09 +0100

    ADR-2063 Remove test-only route to create and fill user answers

--- app/uk/gov/hmrc/alcoholdutycontactpreferences/testOnly/controllers/TestOnlyUserAnswersController.scala
+++ app/uk/gov/hmrc/alcoholdutycontactpreferences/testOnly/controllers/TestOnlyUserAnswersController.scala
@@ -16,81 +16,22 @@
 
 package uk.gov.hmrc.alcoholdutycontactpreferences.testOnly.controllers
 
-import org.apache.pekko.util.ByteString
-import play.api.Logging
-import play.api.http.HttpEntity
-import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.mvc._
-import uk.gov.hmrc.alcoholdutycontactpreferences.connectors.SubscriptionConnector
-import uk.gov.hmrc.alcoholdutycontactpreferences.controllers.actions.{AuthorisedAction, CheckAppaIdAction}
-import uk.gov.hmrc.alcoholdutycontactpreferences.models.{DecryptedUA, UserAnswers, UserDetails}
 import uk.gov.hmrc.alcoholdutycontactpreferences.repositories.UserAnswersRepository
-import uk.gov.hmrc.crypto.Sensitive.SensitiveString
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import uk.gov.hmrc.play.bootstrap.backend.http.ErrorResponse
 
-import java.time.Clock
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 class TestOnlyUserAnswersController @Inject() (
   cc: ControllerComponents,
-  userAnswersRepository: UserAnswersRepository,
-  subscriptionConnector: SubscriptionConnector,
-  authorise: AuthorisedAction,
-  checkAppaId: CheckAppaIdAction,
-  clock: Clock
+  userAnswersRepository: UserAnswersRepository
 )(implicit ec: ExecutionContext)
-    extends BackendController(cc)
-    with Logging {
+    extends BackendController(cc) {
 
   def clearAllData: Action[AnyContent] = Action.async { _ =>
     for {
       _ <- userAnswersRepository.collection.drop().toFuture()
     } yield Ok("All data cleared")
   }
-
-  def createAndFillUserAnswers(verified: Boolean, locked: Boolean): Action[JsValue] =
-    authorise(parse.json).async { implicit request =>
-      withJsonBody[UserDetails] { userDetails =>
-        val appaId = userDetails.appaId
-
-        checkAppaId(appaId).invokeBlock[JsValue](
-          request,
-          { implicit request =>
-            val subscriptionContactPreferences = subscriptionConnector.getSubscriptionContactPreferences(appaId)
-            subscriptionContactPreferences.foldF(
-              err => {
-                logger.warn(
-                  s"Unable to get existing contact preferences for $appaId - status ${err.statusCode}"
-                )
-                Future.successful(error(err))
-              },
-              contactPreferences => {
-                val emptyUserAnswers: UserAnswers = UserAnswers.createUserAnswers(
-                  userDetails = userDetails,
-                  contactPreferences = contactPreferences,
-                  clock = clock
-                )
-                val submittedEmail                = (verified, locked) match {
-                  case (true, false) => "john.doe@example.com"
-                  case (true, true)  => "jane.doe2@example.com"
-                  case _             => "jane.doe@example.com"
-                }
-                val userAnswers                   = emptyUserAnswers.copy(
-                  emailAddress = Some(SensitiveString(submittedEmail)),
-                  data = JsObject(Seq("contactPreferenceEmail" -> Json.toJson(true)))
-                )
-                userAnswersRepository.add(userAnswers).map(ua => Created(Json.toJson(DecryptedUA.fromUA(ua))))
-              }
-            )
-          }
-        )
-      }
-    }
-
-  def error(errorResponse: ErrorResponse): Result = Result(
-    header = ResponseHeader(errorResponse.statusCode),
-    body = HttpEntity.Strict(ByteString(Json.toBytes(Json.toJson(errorResponse))), Some("application/json"))
-  )
 }
