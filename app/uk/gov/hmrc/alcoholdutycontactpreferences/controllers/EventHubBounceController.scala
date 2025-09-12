b935a8194dc36482cfc06d290a33522684f972d2
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2025-09-12 11:25:28 +0100

    ADR-2252: Code review comment

--- app/uk/gov/hmrc/alcoholdutycontactpreferences/controllers/EventHubBounceController.scala
+++ app/uk/gov/hmrc/alcoholdutycontactpreferences/controllers/EventHubBounceController.scala
@@ -37,7 +37,7 @@ class EventHubBounceController @Inject() (
     request.body
       .validate[EmailBouncedEvent]
       .fold(
-        invalid = error => {
+        invalid = _ => {
           logger.warn(s"Bounced email json body could not be parsed as EmailBouncedEvent")
           Future.successful(BadRequest("Bounced email json body could not be parsed as EmailBouncedEvent"))
         },
