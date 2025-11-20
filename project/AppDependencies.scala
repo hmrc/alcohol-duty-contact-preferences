133aceb950c74794d6b0e81299f4cae225bed338
ajchilts <40767309+ajchilts@users.noreply.github.com>
2025-11-20 10:42:33 +0000

    ADR-2336 | Fix PR bot coment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"
+    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0" % Test
   )
 
   val itDependencies = Seq.empty
