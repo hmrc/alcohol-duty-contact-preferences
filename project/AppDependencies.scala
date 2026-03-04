452bf32599cc103a52309886b2e4b2df29afc680
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-04 10:25:01 +0000

    Update deps + plugins to address security vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "com.beachape"      %% "enumeratum"                % "1.9.0",
-    "com.beachape"      %% "enumeratum-play"           % "1.9.0",
-    "com.beachape"      %% "enumeratum-cats"           % "1.9.0"
+    "com.beachape"      %% "enumeratum"                % "1.9.5",
+    "com.beachape"      %% "enumeratum-play"           % "1.9.5",
+    "com.beachape"      %% "enumeratum-cats"           % "1.9.5"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"       % Test
   )
 
-  val itDependencies = Seq.empty
+  val itDependencies: Seq[Nothing] = Seq.empty
 }
